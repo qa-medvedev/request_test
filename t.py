5f0b6346793328baf973418faf7d80186656631d
qa-medvedev <97593117+qa-medvedev@users.noreply.github.com>
2022-12-27 12:36:52 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,8 +1,5 @@
 import requests
-import lxml.html
 
 url = "https://webcom.academy/"
 response = requests.get(url)
-tree = lxml.html.fromstring(response.text)
-title = tree.xpath('//title')[0]
-print(title.text_content())
+print(response.headers)
